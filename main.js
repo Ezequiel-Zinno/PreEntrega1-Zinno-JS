04a1d9329a69e336c349e010c40379f95070858f
Ezequiel <zinnoezequiel7@gmail.com>
2023-10-30 16:45:49 -0300

    commit pre entrega 2

--- main.js
+++ main.js
@@ -36,3 +36,73 @@ for(let turno = 1; turno <= 3; turno++){
 }
 
 alert("La página está saturada por mucha cantidad de pedidos, intente mas tarde");
+
+// Segunda Pre-entrega
+
+     // Funciones matemáticas
+
+
+//     function operacionesMtematicas(){
+//         let numero1 = parseFloat(prompt("Ingrese un número"));
+
+//         let operacion = prompt("Introduce el la operación (+, -, *, /):")
+
+//         let numero2 = parseFloat(prompt("Ingrese otro número"));
+
+//         switch(operacion){
+//             case "+":
+//                 alert("Resultado: " + (numero1 + numero2));
+//                 break;
+
+//             case "-":
+//                 alert("Resultado: " + (nuumero1 - numero2));
+//                 break;
+//             case "*":
+//             alert("Resultado: " + (numero1 * numero2));
+//             break;
+
+//             case "/":
+//                 if(numero2 !==0){
+//                     alert("Resultado " + (numero1 / numero2));
+//                 } else {
+//                     alert("No es posible dividir por cero.");
+//                 }
+//                 break;
+//             default:
+//                 alert("Operacion no valida");
+
+// }
+// }
+
+//     operacionesMtematicas()
+    // !!
+
+
+    //!! objetos
+
+//     let usuario = {
+//         nombre: "Ezequiel",
+//         apellido: "Zinno",
+//         edad: "21"
+// };
+
+// let propiedad = prompt("Ingrese la propiedad que quiere buscar dentro del Array");
+
+// if (!usuario.hasOwnProperty(propiedad)) {
+//     alert("Propiedad no encontrada");
+// }else{
+// alert(usuario[propiedad]);
+// }
+// !!
+
+//!! arrays  
+
+// let numeros = [1, 2, 3, 4, 5 ,6];
+// let index = numeros.indexOf(1);
+// console.log(index);
+
+// let existe = numeros.includes(3);
+// console.log(existe);
+
+// let filtrado = numeros.filter(numero => numero > 1);
+// console.log(filtrado);
